e5ded706ec305ebf6b2c38c3e6cadc4a2a96c16a
HelloCho <skidrow0802@gmail.com>
2020-11-01 11:37:11 +0900

    Error rest api issue

--- SampleUICollectionView/SampleUICollectionView/ViewController.swift
+++ SampleUICollectionView/SampleUICollectionView/ViewController.swift
@@ -6,15 +6,23 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print(Environment.apiKey)
+        loadRecommendPlace()
     }
 
+    private func loadRecommendPlace() {
+        let parameters = ["ServiceKey":Environment.apiKey, "pageNo":"1", "numOfRows":"10", "trrsrtNm":"", "resultType":"json"]
+        // print(parameters)
+        AF.request("http://apis.data.go.kr/3330000/HeaundaeTourAttrInfoService/getTourAttrList?", parameters: parameters, encoder: URLEncodedFormParameterEncoder.default).response { response in
+            // print(response)
+        }
+    }
 
 }
 
